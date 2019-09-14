52d7264e1d61bebcceaea7e66859cbc364f71817
TaurusNova <susilojosafat@gmail.com>
2019-09-14 15:04:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/realtimedatabase/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.realtimedatabase
+
+import android.content.ComponentCallbacks2
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var ref: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ref = FirebaseDatabase.getInstance().getReference("USERS")//Nama tabel = USERS
+
+        btn_submit.setOnClickListener{saveData()}
+        bt_show.setOnClickListener{
+            val i = Intent( this, ShowActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+    private fun saveData() {
+
+        val nama = et_nama.text.toString()
+        val email = et_email.text.toString()
+
+        val userId = ref.push().key.toString()
+        val user = Users(userId, nama, email)
+
+        ref.child(userId).setValue(user).addOnCompleteListener{
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+            et_nama.setText("")
+            et_email.setText("")
+
+            val i = Intent(this, ShowActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/realtimedatabase/Users.kt
@@ -0,0 +1,11 @@
+package com.example.realtimedatabase
+
+//Bentuk objek dengan variabel nama dan email
+
+class Users (var id: String, var nama: String, var email : String){
+
+    constructor():this("","",""){
+
+    }
+
+}
